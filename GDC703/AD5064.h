10cccc6bfc02f36d82aa38983b3cd2d8cdb27e1a
unknown <hxliu_0@sina.com>
2017-12-19 22:58:28 +0800

    ad5064

---
+++ GDC703/AD5064.h
@@ -0,0 +1,15 @@
+/*
+ * AD5064.h
+ *
+ *  Created on: 2017Äê12ÔÂ18ÈÕ
+ *      Author: leo
+ */
+
+#ifndef AD5064_H_
+#define AD5064_H_
+
+
+
+
+
+#endif /* AD5064_H_ */
---
+++ GDC703/Release/AD5064.d
@@ -0,0 +1,19 @@
+# FIXED
+
+AD5064.obj: ../AD5064.c
+AD5064.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+AD5064.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430f47177.h
+AD5064.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+AD5064.obj: C:/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics.h
+AD5064.obj: C:/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics_legacy_undefs.h
+AD5064.obj: ../AD5064.h
+AD5064.obj: ../SPI.h
+
+../AD5064.c: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430f47177.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics_legacy_undefs.h: 
+../AD5064.h: 
+../SPI.h: 
